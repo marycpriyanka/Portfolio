621de10aea06509acd3df25bbf93ed2dd8d29158
Priyanka Mary Chrstine <marycpriyanka@gmail.com>
2022-02-14 11:26:28 -0800

    Shake photo at interval

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,19 @@
-let name = document.getElementsByTagName("h1")[0];
+let nameElement = document.getElementsByTagName("h1")[0];
+let photo = document.getElementById("priyanka");
 
-showName("Priyanka Mary Christine", 0);   
+showName("Priyanka Mary Christine", 0);
+// shakePhoto();
 
 // Displays the name letter by letter with a small delay.
-function showName(myName, index) {   
+function showName(myName, index) {
     if (index < myName.length) {
-      name.append(myName[index++]);
-      setTimeout(function () { showName(myName, index); }, 400);
+        nameElement.append(myName[index++]);
+        setTimeout(function () { showName(myName, index); }, 400);
     }
-  }
+}
+
+const interval = setInterval(shakePhoto, 10000);
+
+function shakePhoto() {
+    $( "#priyanka" ).effect( "bounce", { times: 3 }, "slow" );
+}
